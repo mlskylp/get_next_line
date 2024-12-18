acb01e813d2a6dd9ece1e3136de6d1c803d1ed56
mlskylp <106234905+mlskylp@users.noreply.github.com>
2024-12-18 14:30:20 +0300

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkayaalp <mkayaalp@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 18:58:42 by mkayaalp          #+#    #+#             */
+/*   Updated: 2024/12/18 11:34:03 by mkayaalp         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strchr(char *str, int c);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(char *str);
+
+#endif
